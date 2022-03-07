e347b975a57af7adb7280c30a0dd0775946d2828
Ovissss <obkas123@gmail.com>
2022-03-07 20:52:45 +0000

    nav bar

---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function(){
+    return <h1>Main bit of the page</h1>
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+import logo from "../images/logo192.png"
+
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img className="react_logo" src={logo} alt="React Logo"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course Project-1</h4>
+
+
+
+
+    </nav>)
+}
