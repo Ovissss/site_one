06ad8c016fc8202389e7f6a099fbce21045a128f
Ovissss <obkas123@gmail.com>
2022-04-18 18:11:01 +0100

    finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,18 @@
 import React from "react"
 
-export default function(){
-    return <h1>Main bit of the page</h1>
+export default function Main() {
+    return <main>
+        <h1 className="main--title">Fun facts about React</h1>
+        <ul className="main--list">
+         <li>Was first released in 2013</li>
+         <li>Was originaly created by Jordan Walke</li>
+         <li>Has well over 100k stars on GitHub</li>
+         <li>in maintained by Facebook</li>
+         <li>Powers thousands of enterprise apps, including mobile apps</li>
+
+
+        </ul>
+     
+     
+     </main>
 }
